632a4d60a17bd618de2c095d0f03b6a52f06c0d0
Gabriel Souza Campos <gabriel.s.campos@hotmail.com>
2022-03-19 18:49:07 -0300

    Criando projeto.

---
+++ Editor.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Text;
+
+namespace EditorHtml
+{
+    public static class Editor
+    {
+        public static void Show()
+        {
+            Console.Clear();
+
+            Console.WriteLine("MODO EDITOR");
+            Console.WriteLine("-----------");
+
+            Start();
+        }
+
+        public static void Start()
+        {
+            StringBuilder file = new StringBuilder();
+
+            do
+            {
+                file.Append(Console.ReadLine());
+                file.Append(Environment.NewLine);
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.WriteLine("-----------");
+            Console.WriteLine(" Deseja salvar o arquivo?");
+            Viewer.Show(file.ToString());
+        }
+    }
+}
